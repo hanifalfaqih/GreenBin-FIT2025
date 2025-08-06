c66508e1526adc296ba3723650ea9e361d7f63f4
XIO <100460377+TazkieCT@users.noreply.github.com>
2025-08-06 13:33:02 +0700

    feat: add article detail activity

---
+++ app/src/main/java/id/hanifalfaqih/greenbin_fit2025/ui/article/ArticleDetailActivity.kt
@@ -0,0 +1,12 @@
+package id.hanifalfaqih.greenbin_fit2025.ui.article
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.hanifalfaqih.greenbin_fit2025.R
+
+class ArticleDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
