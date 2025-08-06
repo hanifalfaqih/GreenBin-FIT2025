a2e64f2d23c89be285a39a539c2518594a1cbf6a
Maulana Hanif Al Faqih Rojichan <maulana.rojichan@binus.ac.id>
2025-08-06 15:09:49 +0700

    Create profile fragment

--- app/src/main/java/id/hanifalfaqih/greenbin_fit2025/ui/profile/ProfileFragment.kt
+++ app/src/main/java/id/hanifalfaqih/greenbin_fit2025/ui/profile/ProfileFragment.kt
@@ -6,14 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import id.hanifalfaqih.greenbin_fit2025.R
+import id.hanifalfaqih.greenbin_fit2025.databinding.FragmentProfileBinding
+import id.hanifalfaqih.greenbin_fit2025.viewmodel.UserViewModel
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /**
+         * Get data from view model
+         */
+        userViewModel.getProfile()
+
+        userViewModel.profileData.observe(viewLifecycleOwner) { profileData ->
+            binding.usernameText.text = profileData.name
+            binding.emailText.text = profileData.email
+        }
+    }
 }
