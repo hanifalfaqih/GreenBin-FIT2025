b8385a487fd84ec2ae5fe17fd0a278fceb949d93
Maulana Hanif Al Faqih Rojichan <maulana.rojichan@binus.ac.id>
2025-08-06 14:44:20 +0700

    Create detail article

--- app/src/main/java/id/hanifalfaqih/greenbin_fit2025/ui/article/ArticleDetailActivity.kt
+++ app/src/main/java/id/hanifalfaqih/greenbin_fit2025/ui/article/ArticleDetailActivity.kt
@@ -2,11 +2,44 @@ package id.hanifalfaqih.greenbin_fit2025.ui.article
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import id.hanifalfaqih.greenbin_fit2025.R
+import com.bumptech.glide.Glide
+import id.hanifalfaqih.greenbin_fit2025.databinding.ActivityArticleDetailBinding
+import id.hanifalfaqih.greenbin_fit2025.model.response.article.ArticleDetail
+import id.hanifalfaqih.greenbin_fit2025.viewmodel.ArticleViewModel
 
 class ArticleDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityArticleDetailBinding
+    private lateinit var articleViewModel: ArticleViewModel
+
+    private var content: ArticleDetail? = null
+    private var articleId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_article_detail)
+        binding = ActivityArticleDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        articleId = intent.getIntExtra("ARTICLE_ID", 0)
+        articleId.let {
+            articleViewModel.getDetailArticle(it)
+        }
+
+        articleViewModel.articleDetail.observe(this) { articleDetail ->
+            setContentData(articleDetail)
+        }
+
+    }
+
+    private fun setContentData(articleDetail: ArticleDetail) {
+        content.apply {
+            Glide.with(this@ArticleDetailActivity)
+                .load(articleDetail.image)
+                .into(binding.ivThumbnail)
+
+            binding.tvTitle.text = articleDetail.title
+            binding.tvViewsContent.text = articleDetail.views.toString()
+            binding.tvContent.text = articleDetail.content
+        }
     }
 }
