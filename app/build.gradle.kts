1fcccbe6f03e3dbf482a4a0c5d601b009b7a3320
Maulana Hanif Al Faqih Rojichan <maulana.rojichan@binus.ac.id>
2025-08-06 08:20:22 +0700

    Add libraries and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.android.gms:play-services-vision:20.1.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
+    implementation("androidx.camera:camera-core:1.3.1")
+
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
