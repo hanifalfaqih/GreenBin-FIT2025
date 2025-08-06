44e1108404c751ac622f18d6aa83b8b0c4ecf945
XIO <100460377+TazkieCT@users.noreply.github.com>
2025-08-06 14:02:19 +0700

    feat: add scan QR activity

---
+++ app/src/main/java/id/hanifalfaqih/greenbin_fit2025/ui/send_trash/SendTrashActivity.kt
@@ -0,0 +1,15 @@
+package id.hanifalfaqih.greenbin_fit2025.ui.send_trash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.hanifalfaqih.greenbin_fit2025.R
+
+class SendTrashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_trash)
+    }
+}
